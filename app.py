f7baeabdee2a728ef7c1b97cd6fcaef6f22330f2
PGR <mynameiswin9@gmail.com>
2025-12-23 19:19:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -121,19 +121,15 @@ st.dataframe(feeder_df.round(6), use_container_width=True)
 st.header("5. Collector Equivalent Results")
 
 collector_df = pd.DataFrame({
-    "Parameter": ["R1 (Ω)", "X1 (Ω)", "B1 (S)", "R0 (Ω)", "X0 (Ω)", "B0 (S)"],
-    "Ohmic / SI Value": [
-        R1_eq, X1_eq, B1_eq,
-        R0_eq, X0_eq, B0_eq
-    ],
-    "Per Unit (pu)": [
-        R1_eq / Z_base,
-        X1_eq / Z_base,
-        B1_eq / B_base,
-        R0_eq / Z_base,
-        X0_eq / Z_base,
-        B0_eq / B_base
-    ]
+   "Parameter": ["R1 (Ω)", "X1 (Ω)", "B1 (µS)", "R0 (Ω)", "X0 (Ω)", "B0 (µS)"],
+"Ohmic / SI Value": [
+    R1_eq,
+    X1_eq,
+    B1_eq * 1e6,
+    R0_eq,
+    X0_eq,
+    B0_eq * 1e6
+],
 })
 
 st.dataframe(collector_df.round(6), use_container_width=True)
